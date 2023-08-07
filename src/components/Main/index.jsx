d948158a7d390383cfb5c694b162a16fc88efa59
Nahuel Medina <nahuelmedina738@gmail.com>
2023-08-07 00:38:21 -0300

    t

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import HomeCard from '../HomeCard'
 import style from './style.css'
+
+
 export default function Main() {
     return (
         <main className='container'>
             <HomeCard />
+
         </main>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Main from '../components/Main'
 import Stepper from '../components/Stepper'
 
+
 export default function Home() {
     return (
         <>
             <Stepper />
             <Main />
-
+            {/* <Carrousel /> */}
         </>
     )
 }
